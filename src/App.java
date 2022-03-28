a91635f99ef3121b2825425afe431746709c5044
lai2-01 <lai2-01@itismeucci.com>
2022-03-28 13:14:13 +0200

    setteNani

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SetteNani t1 = new SetteNani("pisolo");
+        SetteNani t2 = new SetteNani("mammolo");
+        SetteNani t3 = new SetteNani("brontolo");
+        SetteNani t4 = new SetteNani("cucciolo");
+        SetteNani t5 = new SetteNani("gongolo");
+        SetteNani t6 = new SetteNani("eolo");
+        SetteNani t7 = new SetteNani("dotto");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
+
+
     }
 }
